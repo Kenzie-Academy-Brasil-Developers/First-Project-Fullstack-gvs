fb2c63753b58f1debe0d90d04c7aff368861dac2
Gustavo Medeiros <100388082+gusmfs@users.noreply.github.com>
2024-03-12 15:17:35 -0300

    Delete dist directory

--- dist/src/middlewares/dataIsValid.middleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dataIsValidMiddleware = void 0;
-const dataIsValidMiddleware = (schema) => (req, res, next) => {
-    const validatedBody = schema.parse(req.body);
-    req.body = validatedBody;
-    return next();
-};
-exports.dataIsValidMiddleware = dataIsValidMiddleware;
--- dist/src/repositories.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.contactsRepository = exports.clientRepository = void 0;
-const data_source_1 = require("./data-source");
-const client_entity_1 = require("./entities/client.entity");
-const contacts_entity_1 = require("./entities/contacts.entity");
-exports.clientRepository = data_source_1.AppDataSource.getRepository(client_entity_1.Client);
-exports.contactsRepository = data_source_1.AppDataSource.getRepository(contacts_entity_1.Contact);
